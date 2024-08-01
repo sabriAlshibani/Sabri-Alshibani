5ba9655d8f2ab7c5f3f16f636b1cbfc3397db09c
sabriAlshibani <sbryalshybany1@gmail.com>
2024-08-02 01:50:28 +0300

    add part about

--- main.js
+++ main.js
@@ -20,6 +20,15 @@ menu.onclick = function () {
 function loading() {
     let save = localStorage.getItem('color-mode') || 'dark-mode';
     document.body.className = save;
+    localStorage.setItem('dark','bx bx-sun');
+    localStorage.setItem('light','bx bx-moon');
+    var dark = localStorage.getItem('dark');
+    var light = localStorage.getItem('light');
+    if (document.body.classList.contains("dark-them")) {
+        icon.className = dark;
+    } else {
+        icon.className = light;
+    }
 }
 function change() {
     var icon = document.getElementById("icon");
